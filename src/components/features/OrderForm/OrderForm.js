f361e5e07599b832ef236f58bebe23481df73d31
Katarzyna <kkazimierczuk@gmail.com>
2020-11-02 13:45:15 +0100

    prepare OrderForm view

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import { connect } from 'react-redux';
+// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+
+import styles from './OrderForm.module.scss';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const Component = ({submitForm, orderData, setOrderDate, handleChange}) => (
+  <form
+    className={styles.form}
+    onSubmit={submitForm}
+  >
+    <TextField
+      id="name"
+      label="Name"
+      variant="outlined"
+      InputProps={{
+        minLength: 10,
+      }}
+      required
+      fullWidth
+      className={styles.formFieldFullWidth}
+      onChange={handleChange}
+    />
+    <TextField
+      id="surname"
+      label="Surname"
+      variant="outlined"
+      InputProps={{
+        minLength: 10,
+      }}
+      required
+      fullWidth
+      className={styles.formFieldFullWidth}
+      onChange={handleChange}
+    />
+    <TextField
+      id="email"
+      label="Email"
+      variant="outlined"
+      type="email"
+      required
+      fullWidth
+      className={styles.formFieldFullWidth}
+      onChange={handleChange}
+    />
+    <TextField
+      id="telephone"
+      label="Phone number"
+      variant="outlined"
+      type="tel"
+      className={styles.formFieldPartialWidth}
+      onChange={handleChange}
+    />
+    <Button
+      variant="outlined"
+      color="primary"
+      size="large"
+      className={styles.button}
+      type="submit"
+      onClick={setOrderDate}
+    >
+      Submit Order Form
+    </Button>
+  </form>
+);
+
+Component.propTypes = {
+  submitForm: PropTypes.func,
+  orderData: PropTypes.object,
+  setOrderDate: PropTypes.func,
+  handleChange: PropTypes.func,
+};
+
+// const mapStateToProps = state => ({
+//   someProp: reduxSelector(state),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   someAction: arg => dispatch(reduxActionCreator(arg)),
+// });
+
+// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+
+export {
+  Component as OrderForm,
+  // Container as OrderForm,
+  Component as OrderFormComponent,
+};
