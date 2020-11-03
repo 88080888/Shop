c274cee7f6226805a9648ee758978de346d76bcd
Katarzyna <kkazimierczuk@gmail.com>
2020-11-03 12:26:53 +0100

    add ProductCounter

--- src/components/features/CartLogo/CartLogo.js
+++ src/components/features/CartLogo/CartLogo.js
@@ -2,14 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import Button from '@material-ui/core/Button';
+import { getAllCartProducts } from '../../../redux/cartRedux';
 
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './CartLogo.module.scss';
 
+import Button from '@material-ui/core/Button';
+
 class Component extends React.Component {
+
+  static propTypes = {
+    cartProducts: PropTypes.array,
+  }  
+
+  cartProductCounter() {
+    const { cartProducts } = this.props;
+    let totalQuantity = 0;
+    cartProducts.map(cartProduct => totalQuantity += cartProduct.quantity);
+    return totalQuantity;
+  }
 
   render() {
     return(
@@ -17,7 +30,7 @@ class Component extends React.Component {
         <Button className={styles.link} component={Link} to={`/cart`}>
           <ShoppingCartIcon className={styles.icon} />
         </Button>
-        <span className={styles.productCounter}>(ilość)</span>
+        <span className={styles.productCounter}>{this.cartProductCounter()}</span>
       </div>
     );
   }
@@ -28,18 +41,18 @@ Component.propTypes = {
   className: PropTypes.string,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  cartProducts: getAllCartProducts(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as CartLogo,
-  // Container as CartLogo,
+  //Component as CartLogo,
+  Container as CartLogo,
   Component as CartLogoComponent,
 };
