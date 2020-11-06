cf0eb7e6c9d4cac96eaaa1d169a4665be59a902e
Katarzyna <kkazimierczuk@gmail.com>
2020-11-06 18:20:22 +0100

    move front-end part to client file

---
+++ client/build/precache-manifest.a8ea6dcd0efe971d446afc2ee737c602.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447a2feb1d652587fba4a368dd2a128d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38d8111ebd794936b695",
+    "url": "/static/css/main.9dbb5d25.chunk.css"
+  },
+  {
+    "revision": "ebeeca910265c5b2561d",
+    "url": "/static/js/2.4d346d1f.chunk.js"
+  },
+  {
+    "revision": "38d8111ebd794936b695",
+    "url": "/static/js/main.1ad8711c.chunk.js"
+  },
+  {
+    "revision": "9e32521b87c1e23774a5",
+    "url": "/static/js/runtime-main.b9c013c5.js"
+  }
+]);
---
+++ client/src/components/views/Homepage/Homepage.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ProductList } from '../../features/ProductList/ProductList';
+import { connect } from 'react-redux';
+import { getAllProducts } from '../../../redux/productsRedux';
+
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+
+// import { connect } from 'react-redux';
+// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+
+import styles from './Homepage.module.scss';
+
+class Component extends React.Component {
+
+  static propTypes = {
+    products: PropTypes.array,
+  }  
+  
+  render() {
+    const { products } = this.props;
+
+    return(
+      <Paper>
+        <Grid container  className={styles.gridContainer} justify='center'>
+          <Grid item xs={12}>
+            <ProductList products={products}/>
+          </Grid>
+        </Grid>
+      </Paper>
+    );
+  }
+}
+
+
+const mapStateToProps = state => ({
+  products: getAllProducts(state),
+});
+
+// const mapDispatchToProps = dispatch => ({
+//   someAction: arg => dispatch(reduxActionCreator(arg)),
+// });
+
+const Container = connect(mapStateToProps)(Component);
+
+export {
+  //Component as Homepage,
+  Container as Homepage,
+  Component as HomepageComponent,
+};
